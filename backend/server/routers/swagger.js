f26c0f5616d26c839731f91a1c7e5702e429008f
Simon Priadka <manik949@gmail.com>
2019-05-12 17:22:04 +0200

    introduced serverless

--- backend/server/routers/swagger.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-module.exports = function(app){
-  const router = express.Router();
-  app.use('/swagger/api', express.static('./public/swagger.yaml'));
-  app.use('/explorer', express.static('./public/swagger-ui'));
-  app.use(router);
-}
