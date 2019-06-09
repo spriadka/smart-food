75f3f6c0ef7d4e3351b72df7d046a2f7de7d0226
Simon Priadka <manik949@gmail.com>
2019-06-09 08:38:38 +0200

    modified populate data

--- model/src/food-entry.ts
+++ model/src/food-entry.ts
@@ -1,15 +1,22 @@
 import { NutritionInformation } from "./nutrition-information";
+import { Mass } from "./mass";
 
 export class FoodEntry {
   name: string;
   brand: string;
+  amount: Mass
   nutritionInfo: NutritionInformation;
+  dateOfPurchase: Date;
+  expirationDate: Date;
   images: URL[];
 
-  constructor(_name: string, _brand: string, _nutritionInfo: NutritionInformation, _images: URL[]) {
+  constructor(_name: string, _brand: string = "", _amount: Mass = new Mass(0,'g'),_nutritionInfo: NutritionInformation, _images: URL[], _dateOfPurchase: Date = new Date(Date.now()), _expirationDate: Date = new Date(_dateOfPurchase)) {
     this.name = _name;
     this.brand = _brand;
+    this.amount = _amount;
     this.nutritionInfo = _nutritionInfo;
+    this.dateOfPurchase = _dateOfPurchase;
+    this.expirationDate = _expirationDate;
     this.images = _images;
   }
 }
--- sample-data/populate-db.js
+++ sample-data/populate-db.js
@@ -1,4 +1,59 @@
-const Energy = require('smartfood-model/lib/energy').Energy;
+const Cloudant = require('@cloudant/cloudant');
 
-new Energy(10,'g');
+const FoodEntry = require('smartfood-model/lib/food-entry').FoodEntry;
+const Mass = require('smartfood-model/lib/mass').Mass;
+const NutritionInformationBuilder = require('smartfood-model/lib/nutrition-information-builder').NutritionInformationBuilder;
+
+const entries = [
+  new FoodEntry("Banana", "Chiquita", new Mass(100,'g'), new NutritionInformationBuilder()
+  .calories(89,'calories')
+  .carbohydrateContent(23,'g')
+  .proteinContent(1.1,'g')
+  .build(), []),
+  new FoodEntry('Greek White Yogurt 0% fat Milko','Milko', new Mass(140,'g'), new NutritionInformationBuilder()
+  .proteinContent(14,'g')
+  .carbohydrateContent(5,'g')
+  .sugarContent(4,'g')
+  .fatContent(0.42,'g')
+  .saturatedFatContent(0.28,'g')
+  .build(), []),
+  new FoodEntry("Mila Sedita","Sedita", new Mass(50, 'g'), new NutritionInformationBuilder()
+  .proteinContent(5,'g')
+  .calories(1148,'kJ')
+  .carbohydrateContent(24,'g')
+  .sugarContent(17,'g')
+  .fatContent(18,'g')
+  .saturatedFatContent(12,'g')
+  .fiberContent(2,'g')
+  .build(), []),
+  new FoodEntry('Cherry Tomatotes', '', new Mass(100,'g'), new NutritionInformationBuilder()
+  .calories(90,'kJ')
+  .proteinContent(0.9,'g')
+  .carbohydrateContent(4,'g')
+  .fatContent(0.2,'g')
+  .fiberContent(1,'g')
+  .build(),[])
+]
+
+var cloudant = Cloudant({
+  url: process.env.CLOUDANT_URL,
+  username: process.env.CLOUDANT_USERNAME,
+  plugins: {
+    iamauth: {
+      iamApiKey: process.env.IAM_API_KEY
+    }
+  }
+})
+
+cloudant.db.create('sample-data',(err,body) => {
+  if (err) {
+    console.log(err.message)
+  }
+});
+
+const sampleDataDB = cloudant.db.use('sample-data');
+entries.forEach(entry => {
+  sampleDataDB.insert(entry);
+})
+
 
